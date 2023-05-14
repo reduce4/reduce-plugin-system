5dd4d0a71cb8cb63172c35bfc9c8fe704e3ec739
happysmile <812024380@qq.com>
2023-05-15 06:40:13 +0800

    add dsl

--- src/dsl/dsl.js
+++ src/dsl/dsl.js
@@ -20,5 +20,27 @@ export default {
         "config": {
             "token": ""
         }
+    },
+    "@reduce/pdf@0.0.0": {
+        "hooks": {
+            "doSelect": {
+                "input": "doSelect",
+                "inputInitValue": "null",
+                "payloadType": "string",
+                "hooksMeans": "设置选中的内容还有选中的坐标"
+            },
+            "doFloatShow": {
+                "input": "doFloatShow",
+                "inputInitValue": "null",
+                "payloadType": "string",
+                "hooksMeans": "在(x,y)显示悬浮按钮组"
+            },
+            "doFloatButtonClick": {
+                "input": "doFloatButtonClick",
+                "inputInitValue": "0",
+                "payloadType": "number",
+                "hooksMeans": "当浮动按钮点击时"
+            }
+        }
     }
 }
